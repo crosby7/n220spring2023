6b47315ce0c17c6386fb7e85861f0cff7346523d
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-01-26 15:37:28 -0500

    New In Class Exercise
    Replaced the exercise from week 2 with the in class exercise from week 3

--- inClassEx/js/inClassApp.js
+++ inClassEx/js/inClassApp.js
@@ -1,34 +1,57 @@
-let r = 1;
+// Declare a variable that stores the X and Y position of the mouse when it was last clicked
+let lastClickedPos = { x: null, y: null };
 
 
+// In setup, create a canvas
 function setup() {
-    createCanvas(800, 600);
-    background(200);
-    console.log("This is r in setup(): " + r);
+  createCanvas(400, 400);
 }
 
-
-// Draw circles at mouse location. Increase radius r on mouse click
-// Don't allow radius to go above 50
-
-// To achieve this, I'll have a nested if loop first check if r is less than 50. If so,
-// then I can use another if to check if the mouse is pressed. If that is true,
-// then I can increase r by 1. 
 function draw() {
-    console.log("This is the radius in draw: " + r);
-    fill(200, 100, 100);
-    circle(mouseX, mouseY, r);
+    // Use a background to clear old lines
+  background(220);
+  
+  // If mouse is clicked,
+  if(mouseIsPressed) {
+    // Store the x and y in our variable's x and y fields
+    lastClickedPos.x = mouseX;
+    lastClickedPos.y = mouseY;
 
-    if(r < 50) {
-        if(mouseIsPressed) {
-            r++;
-        }
-    }
-    
+    // For debugging, to ensure the variable was overwritten
+    console.log(lastClickedPos.x, lastClickedPos.y);
+  }
+  
+  // Call a function to find the distance between our last clicked position and the current mouse location
+    let lineLength = distance( lastClickedPos, { x: mouseX, y: mouseY });
+
+  // If the length is greater than 120 pixels, 
+  if(lineLength > 120) {
+    // Set the stroke color to red
+    stroke('#FF0000');
+}
+    // Otherwise, 
+  else {
+    // Keep it a default black
+     stroke('#000000');
+   }
+  
+   // After finding our lastClicked position, and determining the distance(for our stroke color), draw a line from lastCLicked positon to current mouse position
+
+   // Since the prompt asks for lines only where the mouse has ALREADY been clicked, I will add an if statement to only draw this line when lastClickedPos is not null
+   if(lastClickedPos.x != null && lastClickedPos.y != null)
+  {
+    line(lastClickedPos.x, lastClickedPos.y, mouseX, mouseY);
+  }
+  
 }
 
-/*
-
-fkjbksjdbckashdkfhaksdjfh
-
-*/
+// This function determines the distance between two 2-dimenstional vectors (lastClicked positon and current mouse Positon)
+function distance(p1, p2) {
+  
+    // First find the difference between the x values and the y values, respectively
+  let dx = p1.x - p2.x;
+  let dy = p1.y - p2.y;
+  
+  //return the distance using the square root of the squares of those differences summed. 
+  return Math.sqrt(dx*dx + dy*dy);
+}
