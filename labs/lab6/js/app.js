fc137a07e7e09a6050f11965297ccefca4300f69
crosby7 <camcrosby09@gmail.com>
2023-02-16 15:39:57 -0500

    Preplanning for lab 6

---
+++ labs/lab6/js/app.js
@@ -0,0 +1,38 @@
+/* Loop but big
+For this, I'll create a for loop, looping between i = 0 and i < 5. Then, I'll use createElement('div') to display (i + 1) * 1000 to the div
+
+
+*/
+
+/* Beep Bop
+
+I'll start with another for loop, iterating between i = 0 and i < 25. 
+Then, I'll create a series of if/elseif statements. If (i % 3 == 0 && i % 5 == 0), I'll display "beepbop" to a created div. Start with this one because it will be skipped if the other two tests are true instead. 
+
+Then, if (i % 3 == 0), I'll display "beep"
+
+If (i % 5 == 0 ), then I'll display "bop"
+
+If none of them return true, just display the iterand i. 
+
+
+
+*/
+
+/* Runner Up
+
+I'll start by declaring and defining an array of 3 runners' best times, in order from lowest to highest.
+
+
+Then, a short for loop, i = 0 to i < 3, will display the indexed times to a created div each iteration. 
+
+
+*/
+
+/* Favorray
+
+I'll start by making a string array of 5 of my favorite things. Then, I'll loop through the array, creating a div that displays the indexed string followed by a predefined string " is one of my favorite things"
+
+
+
+*/
