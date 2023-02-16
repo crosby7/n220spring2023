db97561ae6e5bb7c1d65bff48c591a91f3909de4
crosby7 <camcrosby09@gmail.com>
2023-02-16 16:16:06 -0500

    Completing Lab 6 - with a fun but overbearing random color picker

--- labs/lab6/js/app.js
+++ labs/lab6/js/app.js
@@ -4,6 +4,14 @@ For this, I'll create a for loop, looping between i = 0 and i < 5. Then, I'll us
 
 */
 
+let loopDiv = document.getElementById("loopButBig");
+
+for(i = 0; i < 5; i++)
+{
+    loopDiv.innerHTML += (i + 1) * 1000 + " ";
+}
+
+
 /* Beep Bop
 
 I'll start with another for loop, iterating between i = 0 and i < 25. 
@@ -15,9 +23,34 @@ If (i % 5 == 0 ), then I'll display "bop"
 
 If none of them return true, just display the iterand i. 
 
-
-
 */
+
+let beepBopHeader = document.createElement("h1");
+beepBopHeader.innerHTML = "Beep Bop";
+document.body.appendChild(beepBopHeader);
+
+for(i = 0; i < 25; i++)
+{
+    let beepBopDiv = document.createElement("div");
+    document.body.appendChild(beepBopDiv);
+    if(i % 3 == 0 && i % 5 == 0)
+    {
+        beepBopDiv.innerHTML = "beepbop";
+    }
+    else if(i % 3 == 0)
+    {
+        beepBopDiv.innerHTML = "beep";
+    }
+    else if(i % 5 == 0)
+    {
+        beepBopDiv.innerHTML = "bop";
+    }
+    else
+    {
+        beepBopDiv.innerHTML = i;
+    }
+}
+
 
 /* Runner Up
 
@@ -26,13 +59,54 @@ I'll start by declaring and defining an array of 3 runners' best times, in order
 
 Then, a short for loop, i = 0 to i < 3, will display the indexed times to a created div each iteration. 
 
-
 */
+
+let runnerHeader = document.createElement("h1");
+runnerHeader.innerHTML = "Runner Up";
+document.body.appendChild(runnerHeader);
+
+let runnerTimes = [6.2, 7.4, 9.5];
+
+for(i = 0; i < runnerTimes.length; i++)
+{
+    if(i != 0)
+    {
+        let runnerDiv = document.createElement("div");
+        runnerDiv.innerHTML = runnerTimes[i];
+        document.body.appendChild(runnerDiv);
+    }
+    
+}
 
 /* Favorray
 
 I'll start by making a string array of 5 of my favorite things. Then, I'll loop through the array, creating a div that displays the indexed string followed by a predefined string " is one of my favorite things"
 
-
-
 */
+
+let favHeader = document.createElement("h1");
+favHeader.innerHTML = "Favorray";
+document.body.appendChild(favHeader);
+
+let myFavorray = ["Money", "Gaming", "Food", "Pizza", "Family"];
+
+for(i = 0; i < myFavorray.length; i++)
+{
+    let favDiv = document.createElement("div");
+    favDiv.innerHTML = myFavorray[i] + " is one of my favorite things!";
+    document.body.appendChild(favDiv);
+}
+
+function randomColorPicker() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    return randomColor;
+}
+
+let allDivs = document.getElementsByTagName("div");
+
+for(i = 0; i < allDivs.length; i++)
+{
+    let randomColor = randomColorPicker();
+    allDivs[i].style.backgroundColor = "#" + randomColor;
+}
