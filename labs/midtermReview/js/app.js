68b4ab57cd302bd18aecee482cc099cabae9f269
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-02-23 12:53:03 -0500

    Setting up midterm review and preplanning the UI Highlighter

---
+++ labs/midtermReview/js/app.js
@@ -0,0 +1,26 @@
+/* UI Navigation Highlighter
+
+To make this application, I'll create 7 divs in the HTML file. The first is a holder for all the text of the 6 other clicked divs. 
+#id navCollection
+
+The other 6 can be clicked to highlight them - change background color and highlight text
+When highlighted, the other previously highlighted divs are un-highlighted.
+
+Then, the navCollection div will add the clicked div's text to its innerHTML
+
+To do this, I'll create a function that takes an element as a parameter. This function will be
+called on click in HTML by each of the 6 divs. 
+The function will first set all divs of the class uiNav (the 6 divs) to basic style (no background color, no bold text)
+
+Then the function will take the element given as a parameter and "highlight it". Finally, it will add the text
+of the clicked div to the navCollection
+
+*/
+
+/* Rock Paper Scissors (Guard)
+
+
+
+
+
+*/
