f13b949b8f0f04f406a58d6ce7d1d33d0044be04
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-03-23 15:13:27 -0400

    Completing user Input hw

--- labs/userInput/js/userInput.js
+++ labs/userInput/js/userInput.js
@@ -3,9 +3,7 @@
 To complete this, I will create a text input and a button in HTML. Then, I'll get a reference to the input so I can track its value. 
 Then, in an onclick function call on the button, I'll print "Hello " followed by the value of the input to the console.
  
-
 */
-
 /* Tip Calc
 
 To do this, I'll first make a numeric input and a button in HTML. 
@@ -15,7 +13,6 @@ then calculate a 20% tip and store that as a separate local variable.
 Then, a print to console will display "Tip: " and the tip amount, "Total: " and the bill + tip.
 
 */
-
 /* Password Protected
 
 I'll make two text inputs and one submit button.
@@ -24,5 +21,44 @@ Onclick (on the button), a function will be called that will check the two field
 If the first field says "username" AND the second says "password", then a div on the page
 will be updated to say "Success". If the fields are incorrect, the div will say "Wrong information"
 
-
 */
+
+let greeterInput = document.getElementById("greeterInput");
+let tipCalcInput = document.getElementById("tipCalcInput");
+let usernameInput = document.getElementById("usernameInput");
+let passwordInput = document.getElementById("passwordInput");
+
+let userDiv = document.getElementById("userCheck");
+
+function greeterFunction() {
+    console.log("Hello " + greeterInput.value + "!");
+    greeterInput.value = "";
+}
+
+function tipCalc() {
+    let billSubtotal = Number(tipCalcInput.value);
+    tipCalcInput.value = "";
+
+    let billTip = Math.round(100 *(billSubtotal * .20)) / 100;
+
+    console.log("Tip: $" + billTip + " -- Bill Total: $" + (billSubtotal + billTip));
+}
+
+
+function userCheck() {
+    if(usernameInput.value === "Username" && passwordInput.value === "Password")
+    {
+        userDiv.innerHTML = "Success!";
+    }
+    else if(usernameInput.value === "Username")
+    {
+        userDiv.innerHTML = "Forgot your password?";
+        passwordInput.value = "";
+    }
+    else
+    {
+        userDiv.innerHTML = "User not found";
+        usernameInput.value = "";
+        passwordInput.value = "";
+    }
+}
