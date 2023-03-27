4d5210a7407ba518229e3b421aa4ad39f34ab283
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-03-27 14:21:03 -0400

    Preplanning Functions Exercises

---
+++ labs/functions/js/functions.js
@@ -0,0 +1,29 @@
+/* Is Divisible by Seven
+
+I'll start with an input, button, and div in HTML. 
+I'll get a reference to the input and div in JS, and create an onclick function checkNumber() for the button
+In this function, it will set a local variable givenNumber equal to the value in the input converted to Number (and reset the input to an empty string).
+Then, it will call another function isDivisibleBySeven(givenNumber) which will take the given number as an argument.
+This second function will check if the givenNumber is divisible by seven and return true or false. 
+Back in the checkNumber function, I'll create an if/else that will update the div if the given number is or is not divisible. 
+
+*/
+
+/* Make Random
+
+I'll again start with a button and a div in HTML. 
+Then, I'll get a reference to the div in JS. 
+The button will onclick call a function startRandomizing() that will call function randomGenerator().
+randomGenerator() will create a random whole number between 0 and 10 using Math.floor, and return that number.
+Back in startRandomizing(), the div's innerHTML will be updated to show the randomNumber returned.
+
+*/
+
+/* Show Info
+
+I'll start with three buttons in HTML, labelled "home", "about", and "projects", and a div to show results. 
+Each of these buttons will have an onclick function showInfo(index) that will take a number as an argument (0, 1, and 2 respectively)
+In JS, I'll create an array of strings of the same name ["home", "about", "projects"], and I'll get a reference to the div.
+Then, the function showInfo(index) will update the div with the string indexed by the given numeric parameter.
+
+*/
