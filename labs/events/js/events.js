3a6609f28c4a788ccbf9076a5131341c7d19ff84
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-04-10 21:09:00 -0400

    Completing Events Exercises

--- labs/events/js/events.js
+++ labs/events/js/events.js
@@ -25,3 +25,50 @@ I'll create three buttons in HTML that ask questions. Each will have a data-answ
 An event handler will change the innerHTML of an answerDiv to the data-answer.
 
 */
+
+// Global Variables
+
+// Color Mixer
+let displayDiv = document.getElementById("colorMixerDisplay");
+let textDiv = document.getElementById("colorMixerText");
+let redAmount = 0;
+let greenAmount = 0;
+let blueAmount = 0;
+
+// Flash Cards
+let flashCardsDiv = document.getElementById("flashCards");
+
+
+// Functions - Event handlers
+// Color Changer
+document.querySelectorAll(".squareDiv").forEach(item => {
+    item.addEventListener('click', function(e) {
+        item.style.backgroundColor = item.dataset.color;
+    })
+})
+
+// Color Mixer
+document.querySelectorAll(".colorMixerButton").forEach(item => {
+    item.addEventListener('click', function(e) {
+        if (item.dataset.color === "red")
+        {
+            redAmount += Number(item.dataset.strength);
+        }
+        else if (item.dataset.color === "green")
+        {
+            greenAmount += Number(item.dataset.strength);
+        }
+        else if (item.dataset.color === "blue")
+        {
+            blueAmount += Number(item.dataset.strength);
+        }
+
+        displayDiv.style.backgroundColor = `rgb(${redAmount}, ${greenAmount}, ${blueAmount})`;
+        textDiv.innerHTML = `Current color: rgb(${redAmount}, ${greenAmount}, ${blueAmount})`
+    })
+})
+
+// Flash Cards
+function flashCards(event) {
+    flashCardsDiv.innerHTML = event.target.dataset.answer;
+}
