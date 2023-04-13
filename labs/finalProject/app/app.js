eccaed7f7b7fa6b7f39f0732979a6b2d192147f6
crosby7 <53411767+crosby7@users.noreply.github.com>
2023-04-12 23:17:11 -0400

    Single Page Document MVC implemented
    No game yet, but images uploaded and stored in an array for use when the game board is created

--- labs/finalProject/app/app.js
+++ labs/finalProject/app/app.js
@@ -1,3 +1,38 @@
+// Controller - manipulates data
 
+// On page load, load home page - then create hash listener
 changePage();
 window.addEventListener("hashchange", changePage);
+
+// Onclick in home page to set difficulty of game
+function setDifficulty(difficulty)
+{
+    homeTextDiv = document.getElementById("homeText");
+    gameDifficulty = difficulty;
+
+    if (difficulty === 2)
+    {
+        homeTextDiv.innerHTML = "You selected: Easy";
+    }
+    else if (difficulty === 4)
+    {
+        homeTextDiv.innerHTML = "You selected: Normal";
+    }
+    else
+    {
+        homeTextDiv.innerHTML = "You selected: Hard";
+    }
+}
+
+function startGame()
+{
+    if (gameDifficulty != null)
+    {
+        window.location.hash = "#game";
+        
+        let gameBoard = document.getElementById("gameBoard");
+        console.log(gameBoard);
+    }
+}
+
+
--- labs/finalProject/services/services.js
+++ labs/finalProject/services/services.js
@@ -1,18 +1,119 @@
+// References for the page itself, used for single page document
 let injectorDiv = document.getElementById("injector");
-let currentPage = null;
 
+// Individual HTML pages to be loaded later
+let homePage = `<div class="homePage">
+<h1 class="title">Memory (Concentration)</h1>
+<h3>Select a difficulty:</h3>
+<div class="homeButtons">
+  <button onclick="setDifficulty(2)">Easy</button>
+  <button onclick="setDifficulty(4)">Normal</button>
+  <button onclick="setDifficulty(6)">Hard</button>
+</div>
+<div id="homeText"></div>
+<button class="startGame" onclick="startGame()">Start Game</button>
+</div>`;
+let gamePage = `<h1 class="title">Click to flip a card:</h1>
+<table id="gameBoard"></table>`;
+let winPage = `<h1>Congratulations! You Win!</h1>
+<h3>Play Again?</h3>
+<div class="winScreenButtons">
+  <button>Play Again</button>
+  <button>Back to Menu</button>
+</div>`;
+
+// Game-relevant global variables
+let gameDifficulty = null;
+let score = 0;
+
+// HOME PAGE VARS
+let homeTextDiv;
+
+// GAME VARS
+let easyImages = [
+    "aurora.jpg",
+    "beach.jpg",
+    "aurora.jpg",
+    "beach.jpg"
+];
+let normalImages = [
+    "aurora.jpg",
+    "beach.jpg",
+    "caves.jpg",
+    "closeFronds.jpg",
+    "closeupFlower.jpg",
+    "clouds.jpg",
+    "coastalResort.jpg",
+    "creek.jpg",
+    "aurora.jpg",
+    "beach.jpg",
+    "caves.jpg",
+    "closeFronds.jpg",
+    "closeupFlower.jpg",
+    "clouds.jpg",
+    "coastalResort.jpg",
+    "creek.jpg"
+];
+let hardImages = [
+    "aurora.jpg",
+    "beach.jpg",
+    "caves.jpg",
+    "closeFronds.jpg",
+    "closeupFlower.jpg",
+    "clouds.jpg",
+    "coastalResort.jpg",
+    "creek.jpg",
+    "aurora.jpg",
+    "beach.jpg",
+    "caves.jpg",
+    "closeFronds.jpg",
+    "closeupFlower.jpg",
+    "clouds.jpg",
+    "coastalResort.jpg",
+    "creek.jpg",
+    "desert.jpg",
+    "desertSky.jpg",
+    "distantWaterfall.jpg",
+    "icelandicRainbow.jpg",
+    "jellyfish.jpg",
+    "lakesideMountains.jpg",
+    "lightHouse.jpg",
+    "lightThruTrees.jpg",
+    "midsommar.jpg",
+    "mountainCabin.jpg",
+    "desert.jpg",
+    "desertSky.jpg",
+    "distantWaterfall.jpg",
+    "icelandicRainbow.jpg",
+    "jellyfish.jpg",
+    "lakesideMountains.jpg",
+    "lightHouse.jpg",
+    "lightThruTrees.jpg",
+    "midsommar.jpg",
+    "mountainCabin.jpg"
+];
+
+
+// Function for changing pages
 function changePage() {
     let hashTag = window.location.hash;
     let pageID = hashTag.replace("#", "");
 
-    if (pageID != "")
+    if (pageID === "home")
     {
-        injectorDiv.removeChild(currentPage);
-        injectorDiv.appendChild(`pages/${pageID}.html`);
-        currentPage = `pages/${pageID}.html`;
+        
+        injectorDiv.innerHTML = homePage;
+    }
+    else if (pageID === "game")
+    {
+        injectorDiv.innerHTML = gamePage;
+    }
+    else if (pageID === "winScreen")
+    {
+        injectorDiv.innerHTML = winPage;
     }
     else
     {
-        injectorDiv.appendChild(`pages/home.html`);
+        injectorDiv.innerHTML = homePage;
     }
 }
